9f4688bbafa24aaee5627e8585064d6de546bea4
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-30 10:32:54 +0330

    Change Something in layout view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,11 +53,11 @@
               <li class="nav-item dropdown">
                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                   data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }}
+                  Hi, {{ Auth::user()->name }} 👋
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('logout') }}"
+                  <a class="dropdown-item text-danger" href="{{ route('logout') }}"
                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
                   </a>
