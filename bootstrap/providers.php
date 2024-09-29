605f3d4885ddbdc48975ab68cb167b53b2e4e684
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-09-29 18:46:55 +0330

    Remove blade-service-provider and helpers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\BladeServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
