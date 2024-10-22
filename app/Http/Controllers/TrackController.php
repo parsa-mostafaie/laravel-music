05fc30ecf248e8dc9e7e8240e43ebb134f445999
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-10-22 15:45:43 +0330

    [WIP]

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -93,11 +93,14 @@ class TrackController extends Controller
     return $track->publish();
   }
 
-  public function listen(Request $request, Track $track, ?string $slug=null){
+  public function listen(Request $request, Track $track, ?string $slug = null)
+  {
 
     if ($slug !== $track->slug) {
       return redirect(url($track->listen_url, $request->all()));
     }
+
+    $track->load('artist', 'category');
 
     return inertia('Tracks/Single', compact('track'));
   }
--- resources/js/Components/TrackListen.vue
+++ resources/js/Components/TrackListen.vue
@@ -1,19 +1,36 @@
 <template>
   <!-- This is an example component -->
   <div class="p-3 flex justify-center">
-    <Card class="flex-col">
+    <Card class="flex-col max-w-full sm:max-w-[75%] md:max-w-[50%]">
       <template #image v-if="track.image_url">
-        <img class="d-block rounded-lg h-[200px]" :src="track.image_url" alt="Image" />
+        <img
+          class="d-block rounded-lg max-w-full"
+          :src="track.image_url"
+          alt="Image"
+        />
       </template>
       <template #header>
-          <h5>
-            {{ track.name }}
-          </h5>
+        <div class="flex font-normal justify-between items-center">
+          <span class="font-bold">{{ track.name }}</span>
+          <a
+            :href="track.artist.profile_url"
+            class="flex items-center text-slate-400 text-base gap-1"
+          >
+            <img
+              :src="track.artist.image_url"
+              class="h-5 w-auto rounded-lg object-cover"
+              alt="Artist Profile"
+            />
+            {{ track.artist.name }}
+          </a>
+        </div>
       </template>
       <p class="sm:max-w-[90%] text-justify">
-        <template v-if="track.bio">{{ track.bio }}</template>
-        <i v-else>No Biography is Provided!</i>
+        <template v-if="track.summary">{{ track.summary }}</template>
+        <i v-else>No Summary is Provided!</i>
       </p>
+      <p class="text-xl text-slate-300 font-bold">Lyric</p>
+      {{ track.lyric }}
     </Card>
   </div>
 </template>
